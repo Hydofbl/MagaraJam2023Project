3f06655863db4a0751f7852bf6b85fd4ca4a3618
JystXen <oglumoguz@hotmail.com>
2023-11-17 23:18:42 +0300

    Update: Double click, Virus window added

---
+++ Assets/Scripts/Automation/TimeController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField] TMP_Text currentTime;
+
+    private void Update()
+    {
+        
+    }
+}
